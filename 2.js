0ad5291ec6a2054add4bbb834f5b7eb15a43afa9
Rios06 <jenniriioss4@gmail.com>
2023-08-10 10:05:43 -0500

    algo

---
+++ 2.js
@@ -0,0 +1,7 @@
+let semestre1 = prompt("Ingrese nota del primer semestre");
+let semestre2 = prompt("Ingrese nota del tercer semestre");
+let semestre3 = prompt("Ingrese nota del cuarto semestre");
+console.log("el 25% de la calificacion 1 es " + semestre1*.25);
+console.log("el 25% de la calificacion 2 es " + semestre2*.25);
+console.log("el 50% de la calificacion 3 es " + semestre3*.50);
+console.log("El promedio total es " + semestre1*.25+semestre2*.25+semestre3*.50);
